17c4b7f4fefd84c7aaf316170275c94902f28b6f
Patrykkbbb <ppatryk8@op.pl>
2024-08-01 21:47:41 +0200

    second tests rev, files tested by Ruff

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from random import randint
 import argparse
 import os
+
+
 def tries():
     attempts = 0
     parser = argparse.ArgumentParser(description="Gra: Zgadnij losową liczbę")
-    parser.add_argument('--max-attempts', type=int, default=5, help=('- Maksymalna cyfra to 5'))
-    parser.add_argument('--max_number', type=int, default=100, help=('- Zakres górny wynosi 100'))
-    parser.add_argument('--min_number', type=int, default=0, help=('- Zakres dolny zakres wynosi 1'))
+    parser.add_argument(
+        "--max-attempts", type=int, default=5, help=("- Maksymalna cyfra to 5")
+    )
+    parser.add_argument(
+        "--max_number", type=int, default=100, help=("- Zakres górny wynosi 100")
+    )
+    parser.add_argument(
+        "--min_number", type=int, default=0, help=("- Zakres dolny zakres wynosi 1")
+)
+
     args = parser.parse_args()
     max_attempts: int = args.max_attempts
     min_number: int = args.min_number
     max_number: int = args.max_number
     args = parser.parse_args()
 
-    return attempts,max_attempts,args,max_number,min_number
-def limit_numbers(max_number,min_number):
-    attempts,max_attempts,args,max_number,min_number = tries()
-    max_number = 100
-    min_number = 0
-if __name__ == '__limit_numbers__':
-    limit_numbers()
-def win(win_game, clear_screen = False) -> str:
+    return attempts, max_attempts, args, max_number, min_number
+def win(win_game, clear_screen=False) -> str:
     def clear_screen():
-        clear = os.system('cls')
+        os.system("cls")
 
     win_game: str = input("Czy chcesz zagrać jeszcze raz? y/n ")
     if win_game == "y":
@@ -37,14 +40,15 @@ def win(win_game, clear_screen = False) -> str:
         print("Koniec Gry")
         print("-" * 20)
     return win_game
+
+
 def main():
-    attempts,max_attempts,args,max_number,min_number = tries()
-    number_drawn: int = randint(1,100)
+    attempts, max_attempts, args, max_number, min_number = tries()
+    number_drawn: int = randint(1, 100)
     print("-" * 20)
     print("GRA: Znajdz wylosowaną liczbę")
     print("-" * 20)
     while True:
-
         try:
             guess_number: int = input("Podaj liczbę która została wylosowana: ")
             print(f"\n - Pozostało {max_attempts} prób \n")
@@ -58,22 +62,25 @@ def main():
             if int(guess_number) < min_number:
                 print(" - Zakres dolny wynosi 1! \n")
         except ValueError as e:
-                print(" - Podano złą wartość*")
-                print(" - Wymaganą wartością jest liczba*")
-                print(f"{e}")
-                return win(win_game="")
+            print(" - Podano złą wartość*")
+            print(" - Wymaganą wartością jest liczba*")
+            print(f"{e}")
+            return win(win_game="")
         finally:
             max_attempts -= 1
         if number_drawn == int(guess_number):
-            print(f"Brawo! To ta liczba!\n")
-            print("-"*20, f"\n - Wylosowana liczba to: {number_drawn} | Liczba prób: {attempts}")
+            print("Brawo! To ta liczba!\n")
+            print(
+                "-" * 20,
+                f"\n - Wylosowana liczba to: {number_drawn} | Liczba prób: {attempts}",
+            )
             return win(win_game="")
         if max_attempts < 0:
-            print("-" *20, '\n')
-            print(f"Wykorzystano Wszystkie próby \n")
+            print("-" * 20, "\n")
+            print("Wykorzystano Wszystkie próby \n")
             print(f"Liczba która była wylosowana to: {number_drawn}")
-            print('\n', "-" *20,'\n')
+            print("\n", "-" * 20, "\n")
             return win(win_game="")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,17 +1,20 @@
 def win(win_game):
-    return win_game in ["y","n"]
+    return win_game in ["y", "n"]
+
 def test_main():
     assert win("y") is True
     assert win("n") is True
     assert win("q") is False
-def limit_numbers(max_number,min_number):
+
+def limit_numbers(max_number, min_number):
     if max_number > 100:
         return False
     if min_number < 0:
         return False
     else:
         return True
-def test_main():
-    assert limit_numbers(99,25) is True
-    assert limit_numbers(150,-3) is False
-    assert limit_numbers(150,0) is False
+
+def test_main2():
+    assert limit_numbers(99, 25) is True
+    assert limit_numbers(150, -3) is False
+    assert limit_numbers(150, 0) is False
