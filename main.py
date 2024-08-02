536a13b691e1ccf081f668a7fe0bb7dafe974166
Patrykkbbb <ppatryk8@op.pl>
2024-08-02 17:39:03 +0200

    corrected code and created pyproject.toml

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
-from random import randint
 import argparse
 import os
+from random import randint
 
 
-def tries():
-    attempts = 0
+def parse_args():
     parser = argparse.ArgumentParser(description="Gra: Zgadnij losową liczbę")
     parser.add_argument(
-        "--max-attempts", type=int, default=5, help=("- Maksymalna cyfra to 5")
+        "--max-attempts",
+        type=int,
+        default=5,
+        help=("- Maksymalna cyfra to 5"),
     )
     parser.add_argument(
-        "--max_number", type=int, default=100, help=("- Zakres górny wynosi 100")
+        "--max_number",
+        type=int,
+        default=100,
+        help=("- Zakres górny wynosi 100"),
     )
     parser.add_argument(
-        "--min_number", type=int, default=0, help=("- Zakres dolny zakres wynosi 1")
-)
+        "--min_number",
+        type=int,
+        default=0,
+        help=("- Zakres dolny zakres wynosi 1"),
+    )
+    parser.add_argument("--attempts", type=int, default=0, help=("- Liczba prób"))
 
     args = parser.parse_args()
-    max_attempts: int = args.max_attempts
-    min_number: int = args.min_number
-    max_number: int = args.max_number
-    args = parser.parse_args()
+    return args
 
-    return attempts, max_attempts, args, max_number, min_number
-def win(win_game, clear_screen=False) -> str:
-    def clear_screen():
-        os.system("cls")
 
+def display_message_after_win(win_game, clear_screen=True) -> str:
     win_game: str = input("Czy chcesz zagrać jeszcze raz? y/n ")
+    clear_screen = os.system("cls")
+
     if win_game == "y":
-        # clear_screen()
+        clear_screen
         print("-" * 20)
         print("Zaczynasz od nowa")
         print("-" * 20)
@@ -41,9 +46,8 @@ def win(win_game, clear_screen=False) -> str:
         print("-" * 20)
     return win_game
 
-
 def main():
-    attempts, max_attempts, args, max_number, min_number = tries()
+    args = parse_args()
     number_drawn: int = randint(1, 100)
     print("-" * 20)
     print("GRA: Znajdz wylosowaną liczbę")
@@ -51,36 +55,33 @@ def main():
     while True:
         try:
             guess_number: int = input("Podaj liczbę która została wylosowana: ")
-            print(f"\n - Pozostało {max_attempts} prób \n")
+            print(f"\n - Pozostało {args.max_attempts} prób \n")
             if int(guess_number) > number_drawn:
                 print(" - Podana liczba jest większa niż wylosowana! \n")
             if int(guess_number) < number_drawn:
                 print(" - Podana liczba jest mniejsza niż wylosowana! \n")
-            attempts += 1
-            if int(guess_number) > max_number:
+            args.attempts += 1
+            if int(guess_number) > args.max_number:
                 print(" - Zakres górny wynosi 100! \n")
-            if int(guess_number) < min_number:
+            if int(guess_number) < args.min_number:
                 print(" - Zakres dolny wynosi 1! \n")
         except ValueError as e:
             print(" - Podano złą wartość*")
             print(" - Wymaganą wartością jest liczba*")
             print(f"{e}")
-            return win(win_game="")
+            return display_message_after_win(win_game="")
         finally:
-            max_attempts -= 1
+            args.max_attempts -= 1
         if number_drawn == int(guess_number):
             print("Brawo! To ta liczba!\n")
-            print(
-                "-" * 20,
-                f"\n - Wylosowana liczba to: {number_drawn} | Liczba prób: {attempts}",
-            )
-            return win(win_game="")
-        if max_attempts < 0:
+            print("-" * 20,f"\n - Wylosowana liczba to: {number_drawn} | Liczba prób: {args.attempts}")
+            return display_message_after_win(win_game="")
+        if args.max_attempts < 0:
             print("-" * 20, "\n")
             print("Wykorzystano Wszystkie próby \n")
             print(f"Liczba która była wylosowana to: {number_drawn}")
             print("\n", "-" * 20, "\n")
-            return win(win_game="")
+            return display_message_after_win(win_game="")
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,10 +1,12 @@
 def win(win_game):
     return win_game in ["y", "n"]
 
+
 def test_main():
     assert win("y") is True
     assert win("n") is True
     assert win("q") is False
+
 
 def limit_numbers(max_number, min_number):
     if max_number > 100:
@@ -14,7 +16,8 @@ def limit_numbers(max_number, min_number):
     else:
         return True
 
-def test_main2():
+
+def test_limit_numbers():
     assert limit_numbers(99, 25) is True
     assert limit_numbers(150, -3) is False
     assert limit_numbers(150, 0) is False
